7addab9a1b982046c90785535a7f8a7759807d89
antoniolg <antonio@devexperto.com>
2023-03-01 10:50:38 +0100

    First implementation of BrowseSupportFragment

--- app/src/main/java/com/devepxerto/androidtvtraining/MainFragment.kt
+++ app/src/main/java/com/devepxerto/androidtvtraining/MainFragment.kt
@@ -2,7 +2,9 @@ package com.devepxerto.androidtvtraining
 
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import androidx.leanback.app.BrowseSupportFragment
+import androidx.leanback.widget.*
 
 class MainFragment : BrowseSupportFragment() {
 
@@ -10,5 +12,45 @@ class MainFragment : BrowseSupportFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         title = getString(R.string.browse_title)
+
+        val rowsAdapter = ArrayObjectAdapter(ListRowPresenter())
+        val cardPresenter = CardPresenter()
+
+        (1..5).forEach { categoryId ->
+            val categoryTitle = "Category $categoryId"
+
+            val listRowAdapter = ArrayObjectAdapter(cardPresenter).apply {
+                addAll(0, (1..10).map { "Title $it" })
+            }
+
+            val header = HeaderItem(categoryId.toLong(), categoryTitle)
+            rowsAdapter.add(ListRow(header, listRowAdapter))
+        }
+
+        adapter = rowsAdapter
     }
+}
+
+class CardPresenter : Presenter() {
+
+    override fun onCreateViewHolder(parent: ViewGroup): ViewHolder {
+
+        val cardView = ImageCardView(parent.context)
+        cardView.isFocusable = true
+        cardView.isFocusableInTouchMode = true
+
+        return ViewHolder(cardView)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, item: Any) {
+        val title = item as String
+        val cardView = viewHolder.view as ImageCardView
+
+        cardView.titleText = title
+        cardView.setMainImageDimensions(176, 313)
+    }
+
+    override fun onUnbindViewHolder(viewHolder: ViewHolder) {
+    }
+
 }
